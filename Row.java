a8592c7a91314064bd842bdd965b2f0ed10d476d
Fellsmarch <37695784+Fellsmarch@users.noreply.github.com>
2018-04-03 23:17:45 +1200

    Added initial documentation

--- Row.java
+++ Row.java
@@ -1,15 +1,22 @@
+/**
+ * This class creates a row with the following columns:
+ * Postion | Team Name | Wins | Draws | Losses | Goal Difference | Goals Scored | Goals Conceded
+ * The toString method is mainly used for testing, as the final product will be directly put into a GUI
+ * @author Harrison Cook
+ * @version 1.0 03/04/2018
+ */
 
 public class Row {
 	
-	
-	private int wins;
-	private int draws;
-	private int losses;
-	private String teamName;
-	private int tablePosition;
-	private int goalsScored;
-	private int goalsConceded;
-	private int goalDifference;
+	//Need to properly add docstrings for these
+	private int wins; 				//The number of wins a team has
+	private int draws;				//The number of draws a team has
+	private int losses;				//The number of losses a team has
+	private String teamName;		//The team name
+	private int tablePosition;		//The current position the team holds in a/the table
+	private int goalsScored;		//The number of goals the team has scored
+	private int goalsConceded;		//The number of goals the team has conceded
+	private int goalDifference;		//The difference between the number of goals scored and number of goals conceded
 	
 	//Constructor
 	public Row(String name, int position) {
